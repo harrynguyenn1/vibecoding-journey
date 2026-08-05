7620f1a093da92a5c701413d4c7882152f7440b4
Harry Nguyen <nguyenviethai.work@gmail.com>
2026-08-05 21:09:48 +0700

    Phase 1: Set up canvas, draw player square, add game loop

--- game.js
+++ game.js
@@ -1,5 +1,9 @@
 const canvas = document.querySelector("#gameCanvas");
 const ctx = canvas.getContext("2d");
-
-ctx.fillStyle = "blue";
-ctx.fillRect(380, 280, 40, 40);
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "blue";   
+  ctx.fillRect(380, 280, 40, 40);
+  requestAnimationFrame(draw);
+}    
+draw(); 
